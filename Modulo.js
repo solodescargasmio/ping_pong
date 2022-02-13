cf4aaabbaeb0196859ddee4159bba8f2c3ec14d4
solodescargasmio <solodescargasmio@gmail.com>
2022-02-13 17:06:22 -0300

    Movimiento de x,y

--- Modulo.js
+++ Modulo.js
@@ -52,19 +52,7 @@ self.Board.prototype = {
 
 		this.board.bars.push(this);
 		this.kind = "rectangle";
-		this.speed = 10;
 	}
-	self.Bar.prototype={
-		down:function(){
-			this.y += this.speed;
-		},
-		up: function(){
-			this.y -= this.speed;
-		},
-		toString:function(){
-			return "x: "+this.x +" y: "+this.y;
-		}
-	};
 
 
 })();
@@ -97,24 +85,13 @@ self.Board.prototype = {
 		
 	}
 })();
+
+window.addEventListener("load",main)
+function main(){
 var board = new Board(800,400);
 var bar = new Bar(20,100,40,100,board);
 var bar = new Bar(730,100,40,100,board);
 var canvas = document.getElementById("canvas");
 var boar_view = new BoardView(canvas,board);
-
-document.addEventListener("keydown", function(ev){
-	if(ev.keyCode == 38){
-		bar.up();
-	}
-	else if(ev.keyCode == 40){
-		bar.down();
-	}
-	console.log(bar.toString());
-});
-
-window.addEventListener("load",main)
-function main(){
-
     boar_view.draw();
 }
